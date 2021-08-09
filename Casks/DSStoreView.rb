a8f2d4fbdc931620b34265762c8aa541271da16a
Joel Gerhold <joel@gmx.de>
2021-08-09 22:50:51 +0200

    dsstoreview 1.0.0

---
+++ Casks/DSStoreView.rb
@@ -0,0 +1,9 @@
+cask "dsstoreview" do
+  version "1.0.0"
+  desc ""
+  homepage "https://github.com/macmade/DSStoreView/"
+  url "https://github.com/macmade/DSStoreView/releases/download/#{version}/DSStoreView.app.zip"
+  sha256 "bee2b97e383a7c6ff5166dca5d553811cb09f97c81aa803c62ed6ab38ef6f437"
+  name "DSStoreView"
+  app "DSStoreView.app"
+end
